af00e4243cc8abcdd5b35ef46ea73648c41d942e
fred73369 <32518930+fred73369@users.noreply.github.com>
2017-10-04 18:08:14 -0700

    Update config.js

--- app/js/config.js
+++ app/js/config.js
@@ -17,8 +17,8 @@
   let loadMerchantConfiguration = function () {
     let urlConfig = RSBP_CONFIG.system.urlConfig;
     let config = {
-      "name": (urlConfig ? getQueryStringValue("name") : RSBP_CONFIG.payee.name) || "consensusReality",
-      "address": (urlConfig ? getQueryStringValue("address") : RSBP_CONFIG.payee.address) || "1E16XPFWKY2XaFDDzS6V93hGdXP2QEnddY",
+      "name": (urlConfig ? getQueryStringValue("name") : RSBP_CONFIG.payee.name) || "btcchat",
+      "address": (urlConfig ? getQueryStringValue("address") : RSBP_CONFIG.payee.address) || "1hehezYDn95RHFisQkBCf1rNL6p2oJmz1",
       "currency": (urlConfig ? getQueryStringValue("currency") : RSBP_CONFIG.payee.currency) || "BTC",
       "amount": (urlConfig ? getQueryStringValue("amount") : "") || "",
       "discount": (urlConfig ? getQueryStringValue("discount") : RSBP_CONFIG.payee.discount) || 0
